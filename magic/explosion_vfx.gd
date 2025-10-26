b752a0a51827719f36dc9f32295b7cbe347f98fa
myin <minyin142p@gmail.com>
2025-10-26 18:04:30 +0100

    fix explosion not freed

--- magic/explosion_vfx.gd
+++ magic/explosion_vfx.gd
@@ -4,9 +4,7 @@ signal finished()
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func start():
-	animation_player.play("start")
-	animation_player.animation_finished.connect(func(a):
-		if a == "start":
-			finished.emit()
+func _ready() -> void:
+	animation_player.animation_finished.connect(func(_a):
+		finished.emit()
 	)
