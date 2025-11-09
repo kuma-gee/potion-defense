df7206b6dcd9656ca3a43d9045c1fda7bcfddf01
myin <minyin142p@gmail.com>
2025-11-09 10:29:52 +0100

    open recipes in wave setup

--- potion/objects/trash_receiver.gd
+++ potion/objects/trash_receiver.gd
@@ -6,6 +6,5 @@ func _ready() -> void:
 	hovered.connect(func(a: FPSPlayer): label.text = "Throw away" if a and a.has_item() else "")
 	interacted.connect(func(a: FPSPlayer):
 		if a and a.has_item():
-			var item = a.release_item()
-			print("Trashed: %s" % ItemResource.build_name(item))
+			a.release_item()
 	)
